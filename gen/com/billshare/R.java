01c41336aa9ff352e91fe9550c2b6ace2642e6b9
Eric Liu <eric.liu.developer@gmail.com>
2014-05-02 14:11:46 +1000

    68

--- gen/com/billshare/R.java
+++ gen/com/billshare/R.java
@@ -16,9 +16,6 @@ public final class R {
         public static final int abc_slide_out_bottom=0x7f040004;
         public static final int abc_slide_out_top=0x7f040005;
     }
-    public static final class array {
-        public static final int drawer_rows=0x7f0c0000;
-    }
     public static final class attr {
         /**  Custom divider drawable to use for elements in the action bar. 
          <p>Must be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
@@ -1098,23 +1095,28 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f050042;
+        public static final int action_settings=0x7f05004c;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
-        public static final int container=0x7f05003d;
-        public static final int create_member=0x7f050044;
+        public static final int container=0x7f05003c;
+        public static final int create_member=0x7f05004e;
         public static final int default_activity_button=0x7f050028;
         public static final int dialog=0x7f05000e;
         public static final int disableHome=0x7f050008;
         public static final int drawerList=0x7f05003e;
-        public static final int drawer_layout=0x7f05003c;
+        public static final int drawer_layout=0x7f05003d;
         public static final int dropdown=0x7f05000f;
-        public static final int editMember=0x7f050043;
+        public static final int editMember=0x7f05004d;
+        public static final int editText5=0x7f050048;
         public static final int edit_query=0x7f050030;
         public static final int end=0x7f050013;
+        public static final int etEmail=0x7f050047;
+        public static final int etFirstName=0x7f050044;
+        public static final int etLastName=0x7f050045;
+        public static final int etPhone=0x7f050046;
         public static final int expand_activities_button=0x7f050026;
         public static final int expanded_menu=0x7f05002c;
         public static final int home=0x7f050014;
@@ -1148,11 +1150,16 @@ containing a value of this type.
         public static final int split_action_bar=0x7f05001e;
         public static final int submit_area=0x7f050039;
         public static final int tabMode=0x7f050002;
+        public static final int textView1=0x7f050043;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
-        public static final int tvFristName=0x7f050040;
-        public static final int tvId=0x7f05003f;
-        public static final int tvLastName=0x7f050041;
+        public static final int tvAmount=0x7f050040;
+        public static final int tvDueDay=0x7f050042;
+        public static final int tvFristName=0x7f05004a;
+        public static final int tvId=0x7f050049;
+        public static final int tvLastName=0x7f05004b;
+        public static final int tvPaid=0x7f050041;
+        public static final int tvType=0x7f05003f;
         public static final int up=0x7f050021;
         public static final int useLogo=0x7f050003;
         public static final int withText=0x7f05000c;
@@ -1207,15 +1214,20 @@ containing a value of this type.
         public static final int abc_search_dropdown_item_icons_2line=0x7f030015;
         public static final int abc_search_view=0x7f030016;
         public static final int abc_simple_decor=0x7f030017;
-        public static final int activity_main=0x7f030018;
-        public static final int drawer_row=0x7f030019;
-        public static final int fragment_main=0x7f03001a;
-        public static final int member_row=0x7f03001b;
-        public static final int support_simple_spinner_dropdown_item=0x7f03001c;
+        public static final int activity_bill_edit=0x7f030018;
+        public static final int activity_main=0x7f030019;
+        public static final int activity_member_edit=0x7f03001a;
+        public static final int bill_row=0x7f03001b;
+        public static final int drawer_row=0x7f03001c;
+        public static final int fragment_bill_edit=0x7f03001d;
+        public static final int fragment_main=0x7f03001e;
+        public static final int fragment_member_edit=0x7f03001f;
+        public static final int member_row=0x7f030020;
+        public static final int support_simple_spinner_dropdown_item=0x7f030021;
     }
     public static final class menu {
-        public static final int main=0x7f0d0000;
-        public static final int memeber=0x7f0d0001;
+        public static final int main=0x7f0c0000;
+        public static final int memeber=0x7f0c0001;
     }
     public static final class string {
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
@@ -1262,9 +1274,21 @@ containing a value of this type.
         public static final int action_edit_member=0x7f0a0014;
         public static final int action_settings=0x7f0a000f;
         public static final int app_name=0x7f0a000d;
+        public static final int cancel=0x7f0a001d;
+        public static final int comment=0x7f0a001b;
+        public static final int create_bill=0x7f0a001f;
         public static final int drawer_close=0x7f0a0011;
         public static final int drawer_open=0x7f0a0010;
+        public static final int edit_member=0x7f0a0016;
+        public static final int email=0x7f0a001a;
+        public static final int first_name=0x7f0a0017;
         public static final int hello_world=0x7f0a000e;
+        public static final int last_name=0x7f0a0018;
+        public static final int phone_number=0x7f0a0019;
+        public static final int save_member=0x7f0a001c;
+        public static final int title_activity_bill=0x7f0a001e;
+        public static final int title_activity_bill_edit=0x7f0a0020;
+        public static final int title_activity_member_edit=0x7f0a0015;
         public static final int title_activity_memeber=0x7f0a0012;
     }
     public static final class style {
