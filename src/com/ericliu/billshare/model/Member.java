034ae4daffe16e78dc8fb4db89ccd578533f775a
Eric Liu <eric.liu.developer@gmail.com>
2014-05-02 15:53:49 +1000

    temp

--- src/com/ericliu/billshare/model/Member.java
+++ src/com/ericliu/billshare/model/Member.java
@@ -1,6 +1,11 @@
 package com.ericliu.billshare.model;
 
+import com.ericliu.billshare.provider.BillProvider;
+
+import android.content.ContentValues;
 import android.net.Uri;
+
+import static com.ericliu.billshare.provider.DatabaseConstants.*;
 
 public class Member extends Model {
 	
@@ -16,8 +21,24 @@ public class Member extends Model {
 
 	@Override
 	public Uri save() {
+		
+		ContentValues values = new ContentValues();
+		values.put(COL_FIRSTNAME, firstName);
+		values.put(COL_LASTNAME, lastName);
+		values.put(COL_PHONE, phone);
+		values.put(COL_EMAIL, email);
+		
+		
+		Uri uri = null;
+		if (id > 0) {
+			uri = Uri.withAppendedPath(BillProvider.HOUSEMATE_URI, String.valueOf(id));
+			
+			getCr().update(uri, values, null, null);
+		}else{
+			uri = getCr().insert(BillProvider.HOUSEMATE_URI, values);
+		}
 
-		return null;
+		return uri;
 	}
 
 
--- src/com/ericliu/billshare/model/Model.java
+++ src/com/ericliu/billshare/model/Model.java
@@ -1,9 +1,17 @@
 package com.ericliu.billshare.model;
 
+import com.ericliu.billshare.MyApplication;
+
+import android.content.ContentResolver;
 import android.net.Uri;
 
 public abstract class Model {
 	
 	public abstract Uri save();
+	
+	
+	public static ContentResolver getCr(){
+		return MyApplication.getInstance().getContentResolver();
+	}
 
 }
